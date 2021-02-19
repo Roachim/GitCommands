7e676c922fd7f03857598597d48c6d614e183309
Joachim Grubbe Frank <joac1067@stud.kea.dk>
2021-02-19 13:14:52 +0100

    Add project files.

---
+++ GitCommands/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitCommands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GitCommands/temp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitCommands
+{
+    class temp
+    {
+        new int five = 5;
+        new string hey = "hey";
+    }
+}
